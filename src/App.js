4941c31f7f6939c0184d45c78ddea58673a11273
Jaseela.A <jaseelachittur@gmail.com>
2022-10-06 17:57:56 +0530

    completed a line chart component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LineCharter from "./components/LineCharter"
+
+function App() {
+  return (
+    <div className="App">
+     <LineCharter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LineCharter.js
@@ -0,0 +1,38 @@
+import {LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend} from "recharts"
+const data=[
+    {
+        name:"2020",
+        react:32,
+        angular:38
+    },
+    {
+        name:"2021",
+        react:39,
+        angular:48
+    },
+    {
+        name:"2022",
+        react:44,
+        angular:58
+    },
+    {
+        name:"2023",
+        react:40,
+        angular:50
+    },
+]
+
+const LineCharter=()=>{
+    return(
+        <LineChart width={400} height={400} data={data} margin={{ top: 10, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="react" stroke="purple" strokeWidth={3} />
+            <Line type="monotone" dataKey="angular" stroke="blue"/>
+            <CartesianGrid stroke="#ccc"/>
+            <XAxis dataKey="name"/>
+             <YAxis/>
+             <Tooltip />
+             <Legend/>
+            </LineChart>
+    )
+}
+export default LineCharter
